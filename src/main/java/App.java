49cdb7634d65df8ecce952a1eda854c0cedc1809
OHTIJBLEA <kerasin83@gmail.com>
2022-05-19 00:03:01 +0300

    *Cat prototype

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
     }
 }
